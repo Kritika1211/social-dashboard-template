f0ec0729bca4b78c575f79165a7df7d2aec80f27
Kritika1211 <140172129+Kritika1211@users.noreply.github.com>
2025-06-12 23:49:25 +0530

    Delete models.py

--- models.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.models import User
-from django.db import models
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    twitter_token = models.CharField(max_length=255, blank=True)
-    facebook_token = models.CharField(max_length=255, blank=True)
